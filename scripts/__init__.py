7cfc18eedd193eaccbe614ec7ba6eec5fb66b6f7
BrenBook <brendan.lafferty@gmail.com>
2020-07-28 17:46:02 -0700

    missed this in ac76b47 but jupyter notebook also needed an __init__.py in the scripts directory to properly import data_retrieval.py I couldn't help myself and added an 🥚 ... 🙂

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+from random import randrange
+responses = ['data is awesome',
+             'the more you know',
+             'with data comes knowledge,\nwith knowledge understanding\nat least that\'s the hope',
+             "don't be Charles Reep, learn from his mistakes"]
+
+print(responses[randrange(0, len(responses))])
